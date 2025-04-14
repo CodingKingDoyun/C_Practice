efc33d8fed28741dbb3cf701af235d85787fedc0
CodingKingDoyun <codingkingdoyun@gmail.com>
2025-04-14 18:13:10 +0900

    edit 2292.c

--- acmicpc/2292.c
+++ acmicpc/2292.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	int num, floor = 1, cnt = 1;
-	scanf("%d", &num);
+  int num, floor = 1, cnt = 1;
+  scanf("%d", &num);
 
-	while(1) {
-		if(num <= floor) {
-			printf("%d", cnt);
-			break;
-		} else {
-			floor += 6 * cnt;
-			cnt++;
-		}
-	}
+  while (1) {
+    if (num <= floor) {
+      printf("%d", cnt);
+      break;
+    } else {
+      floor += 6 * cnt;
+      cnt++;
+    }
+  }
+  return 0;
 }
--- main/main.c
+++ main/main.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
 
-int function(int num) {
-  for (int i = num - 63; i < num; i++) {
-    int sum = i, tmp = i;
-    while (tmp > 0) {
-      sum += tmp % 10;
-      tmp /= 10;
-    }
-    if (sum == num) return i;
-  }
-  return 0;
-}
-
 int main() {
-  int num;
-  scanf("%d", &num);
-  printf("%d", function(num));
+  int num, max, card[100];
+  scanf("%d %d", &num, &max);
+  for (int i = 0; i < num; i++) {
+    scanf("%d", &card[i]);
+  }
 }
