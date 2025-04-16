57d86b85d7e3846cae216c271a73c34216a2bb2c
CodingKingDoyun <codingkingdoyun@gmail.com>
2025-04-16 15:57:30 +0900

    edit main.c

--- main/main.c
+++ main/main.c
@@ -1,18 +1,29 @@
-// odd > even
 #include <stdio.h>
 
 int main() {
-  int cock[3], even[3] = {0}, res = 1;
-  for (int i = 0; i < 3; i++) {
-    scanf("%d", &cock[i]);
-  }
-  for (int i = 0; i < 3; i++) {
-    if (cock[i] % 2 != 0) {
-      res *= cock[i];
+  int num, price, res = 50001;
+  scanf("%d %d", &num, &price);
+  if (num >= 5) {
+    if (price - 500 < res) {
+      res = price - 500;
     }
   }
-  if (res == 1) {
-    res = cock[0] * cock[1] * cock[2];
+  if (num >= 10) {
+    if (price - (price * 0.1) < res) {
+      res = price - (price * 0.1);
+    }
   }
+  if (num >= 15) {
+    if (price - 2000 < res) {
+      res = price - 2000;
+    }
+  }
+  if (num >= 20) {
+    if (price - (price * 0.25) < res) {
+      res = price - (price * 0.25);
+    }
+  }
+  if (res < 0) res == 0;
   printf("%d", res);
+  return 0;
 }
