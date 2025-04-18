da5b8311bf4ba7d2b42d5b66438f9b28f4926277
CodingKingDoyun <codingkingdoyun@gmail.com>
2025-04-18 15:57:19 +0900

    edit main.c

--- main/main.c
+++ main/main.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
 
 int main() {
-  long long a, b, str, end, cnt;
-  scanf("%lld %lld", &str, &end);
-  if (str > end) {
-    long long tmp = str;
-    str = end;
-    end = tmp;
-  }
-
-  cnt = (end > str + 1) ? end - (str + 1) : 0;
-  printf("%lld\n", cnt);
-  for (long long i = str + 1; i < end; i++) {
-    printf("%lld ", i);
+  int test;
+  scanf("%d", &test);
+  for (int i = 0; i < test; i++) {
+    int wNum, eNum, wArr[31] = {0}, eArr[31] = {0};
+    scanf("%d %d", &wNum, &eNum);
+    for (int j = wNum; j > 0; j--) {
+      int temp = j + eNum--;
+      printf("%d", temp);
+    }
   }
   return 0;
 }
+/*
+2 1
+5 4 3 2 1
+
+7 6 5 4, 5 4 3 2
+
+
+3 2 1
+5 4 3 2 1
+
+8 7 6, 6 5 4, 4 3 2
+
+
+4 3 2 1
+5 4 3 2 1
+
+9 8, 7 6, 5 4, 3 2
+
+4 3 2 1
+6 5 4 3 2 1
+
+10 9 8, 8 7 6, 6 5 4, 4 3 2
+2차원 배열 사용해보기
+*/
